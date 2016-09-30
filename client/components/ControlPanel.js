945d1878ed4457e66cae5b22e82daccbcb81cc01
Kris Gesling <krisgesling@gmail.com>
2016-09-30 11:04:32 +0930

    fix bind error on play btn

--- client/components/ControlPanel.js
+++ client/components/ControlPanel.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const ControlPanel = React.createClass({
   pressPlay() {
-    console.log('Fn: Press play');
     this.props.play()
     this.props.gameTurn(this.props.tiles, this.props.lives);
     this.ctrlFn();
@@ -19,7 +18,7 @@ const ControlPanel = React.createClass({
     return (
 
       <div className="control-panel">
-        <button className="take-turn" onClick={this.pressPlay.bind(null)}>Play</button>
+        <button className="take-turn" onClick={this.pressPlay}>Play</button>
         <button className="take-turn" onClick={this.props.pause.bind(null)}>Pause</button>
         <button className="take-turn" onClick={this.props.faster.bind(null)}>Faster</button>
         <button className="take-turn" onClick={this.props.slower.bind(null)}>Slower</button>
